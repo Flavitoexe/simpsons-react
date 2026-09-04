eecd751e9f95454103345fde1f007723513c078e
Flavio Grilli <flavio.grilli@ynov.com>
2026-09-04 14:51:42 +0200

    added states for donuts count & css styling

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import '../Avatar.css'
+
 interface AvatarProps {
     firstName: string,
     lastName: string,
@@ -5,11 +8,16 @@ interface AvatarProps {
 }
 
 function Avatar({firstName, lastName, imgSrc}: AvatarProps) {
+    const [donuts, setDonuts] = useState(0)
+
     return (
-        <>
+        <div className="avatar">
             <img src={imgSrc} />
-            <p>{firstName} {lastName}</p>
-        </>
+            <div className="lower-avatar">
+                <p>{firstName} {lastName}</p>
+                <button className="donuts-btn" onClick={() => setDonuts(donuts +1)}>🍩{donuts}</button>
+            </div>
+        </div>
     )
 }
 
